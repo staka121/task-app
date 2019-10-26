c99a8a66855c1b16cebd23c2e16d2494d828324a
staka121 <staka121@users.noreply.github.com>
2019-10-26 17:42:22 +0900

    install @atlaskit/css-reset

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import '@atlaskit/css-reset';
 import initialData from './initial-data';
 import Column from './column';
 
