76c47d05ffe94036689ce6500fe47564120a5571
staka <staka121@users.noreply.github.com>
2019-10-31 21:03:09 +0900

    Lessons/react reorder a list with react beautiful dnd (#1)
    * install react-beautiful-dnd
    
    * add drag and drop motion

--- src/task.tsx
+++ src/task.tsx
@@ -1,23 +1,37 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
+import { Draggable } from 'react-beautiful-dnd';
 
 const Container = styled.div`
   border: 1px solid lightgrey;
   border-radius: 2px;
   padding: 8px;
   margin-bottom: 8px;
+  background-color: white;
 `;
 
 interface TaskProps {
   key: string,
   task: any,
+  index: number,
 };
 
 const Task: FC<TaskProps> = ({
   task,
+  index,
 }) => {
   return (
-    <Container>{ task.content }</Container>
+    <Draggable draggableId={ task.id } index={ index }>
+      {(provided) => (
+        <Container
+          ref={ provided.innerRef }
+          { ...provided.draggableProps }
+          { ...provided.dragHandleProps }
+        >
+          { task.content }
+        </Container>
+      )}
+    </Draggable>
   );
 }
 
