1c01f9b7c14cc63949ec862dce9f533b0d9c537d
staka <staka121@users.noreply.github.com>
2019-11-05 08:56:14 +0900

    Lessons/react persist list reordering with react beautiful dnd using the ondragend callback (#2)
    * install react-beautiful-dnd
    
    * add drag and drop motion
    
    * reorder our column

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,55 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import '@atlaskit/css-reset';
-import { DragDropContext } from 'react-beautiful-dnd';
+import { DragDropContext, DropResult } from 'react-beautiful-dnd';
 import initialData from './initial-data';
 import Column from './column';
 
 const App = () => {
-  const [data] = useState(initialData);
+  const [data, updateData] = useState(initialData);
+
+  const onDragEnd = (result: DropResult) => {
+    const { destination, source, draggableId } = result;
+
+    // drops outside of a list
+    if (!destination) {
+      return;
+    }
+
+    // item back into the position that is started
+    // @note
+    //   droppableId is column id.
+    //   index is position into column in item.
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = data.columns[source.droppableId];
+    const newTaskIds = [...column.taskIds];
+
+    // remove item "source" position and insert "destination" position
+    // @note draggableId is dragging item id
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds.flat(),
+    };
+
+    const newState = {
+      ...data,
+      columns: {
+        ...data.columns,
+        [newColumn.id]: newColumn,
+      },
+    };
+
+    updateData(newState);
+  }
 
   const onDragEnd = (result: any) => {
     // TODO: re-order our column
