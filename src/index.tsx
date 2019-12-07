81751ab4c9c954ac5bdfe27c2e8761f34dba7f81
staka121 <staka121@users.noreply.github.com>
2019-12-07 14:47:04 +0900

    [add] change bg color test

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,46 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import '@atlaskit/css-reset';
-import { DragDropContext, DropResult } from 'react-beautiful-dnd';
+import { DragDropContext, DropResult, DragUpdate } from 'react-beautiful-dnd';
 import initialData from './initial-data';
 import Column from './column';
 
 const App = () => {
   const [data, updateData] = useState(initialData);
 
+  const onDragStart = () => {
+    /* ***
+     * You want to change doc text color, if it be dragging item.
+     * Try this code.
+     * 
+     * document.body.style.color = 'DarkBlue';
+     * document.body.style.transition = 'backgrand-color 0.2s ease'
+     */
+  };
+
+  const onDragUpdate = (update: DragUpdate) => {
+    /* ***
+     * You want to change doc background color with relation to item position, if it be dragging item.
+     * Try this code.
+     * 
+     * const { destination } = update;
+     * const opacity = destination
+     *   ? destination.index / Object.keys(data.tasks).length
+     *   : 0;
+     *
+     * document.body.style.backgroundColor = `rgba(153, 141, 217, ${ opacity })`
+     */
+  };
+
   const onDragEnd = (result: DropResult) => {
+    /* ***
+     * You want to reset changed doc color.
+     * Try this code.
+     * 
+     * document.body.style.color = 'inherit';
+     * document.body.style.backgroundColor = 'inherit';
+     */
+
     const { destination, source, draggableId } = result;
 
     // drops outside of a list
@@ -52,7 +84,11 @@ const App = () => {
   }
 
   return (
-    <DragDropContext onDragEnd={ onDragEnd }>
+    <DragDropContext
+      onDragStart={ onDragStart }
+      onDragUpdate={ onDragUpdate }
+      onDragEnd={ onDragEnd }
+    >
       {
         data.columnOrder.map((columnId: string) => {
           const column = data.columns[columnId];
