4ed90da1ec8cffb84911bb8a654149079a91aff9
staka121 <staka121@users.noreply.github.com>
2019-12-11 08:36:28 +0900

    fix task design

--- src/task.tsx
+++ src/task.tsx
@@ -4,23 +4,25 @@ import { Draggable, DraggableProvided, DraggableStateSnapshot } from 'react-beau
 
 interface ContainerProps {
   isDragging: boolean;
-  isDragDisabled: boolean;
 }
 
 const Container = styled.div<ContainerProps>`
   border: 1px solid lightgrey;
-  border-radius: 2px;
+  border-radius: 50%;
   padding: 8px;
-  margin-bottom: 8px;
+  margin-right: 8px;
   background-color: ${ props => (
-    props.isDragDisabled
-      ? 'LightGrey'
-      : props.isDragging
-        ? 'LightCyan'
-        : 'White'
+    props.isDragging
+      ? 'LightCyan'
+      : 'White'
   ) };
 
+
+  width: 40px;
+  height: 40px;
   display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 interface TaskProps {
@@ -29,36 +31,23 @@ interface TaskProps {
   index: number,
 };
 
-// drag handle
-const Handle = styled.div`
-  width: 20px;
-  height: 20px;
-  background-color: orange;
-  border-radius: 4px;
-  margin-right: 8px;
-`;
-
 const Task: FC<TaskProps> = ({
   task,
   index,
 }) => {
-  const isDragDisabled = task.id === 'task-1';
-
   return (
     <Draggable
       draggableId={ task.id }
       index={ index }
-      isDragDisabled={ isDragDisabled }
     >
       {(provided: DraggableProvided, snapshot: DraggableStateSnapshot) => (
         <Container
           ref={ provided.innerRef }
           isDragging={ snapshot.isDragging }
-          isDragDisabled={ isDragDisabled }
+          { ...provided.dragHandleProps }
           { ...provided.draggableProps }
         >
-          <Handle { ...provided.dragHandleProps } />
-          { task.content }
+          { task.content[0] }
         </Container>
       )}
     </Draggable>
